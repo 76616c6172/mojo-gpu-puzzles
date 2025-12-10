e8c6979209828f3af958667a8ee149fcec8e2029
valar <68338647+76616c6172@users.noreply.github.com>
2025-12-09 20:01:26 -0800

    fix docker image

--- modal/app.py
+++ modal/app.py
@@ -1,9 +1,17 @@
 import modal
 
 app = modal.App("76")
-img = modal.Image.from_registry("modular/max-nvidia-full:latest")
+img = (
+  modal.Image.from_registry("nvidia/cuda:12.4.0-base-ubuntu22.04")
+  .apt_install("python-is-python3", "python3-pip")
+  #.pip_install("max", index_url="https://packages.modular.com/max-nightly/pypi/simple")
+  .pip_install("mojo", index_url="https://dl.modular.com/public/nightly/python/simple/", extra_options="--pre")
+)
+
 
 @app.function(image=img)
-def main(): return("Hello from 2025-12-07-modal!")
+def main(): 
+  # Use mojo python interop to call mojo code
+  return("return the result")
 
 
--- modal/call.py
+++ modal/call.py
@@ -1,5 +1,5 @@
 import modal
 
 f = modal.Function.from_name("76", "main")  # app name + function name
-result = f.remote()  # runs remotely, blocks until done, returns the value
+result = f.remote('''call with some mojo gpu puzzle code to run here''')  # runs remotely, blocks until done, returns the value
 print(result)
